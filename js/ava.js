22f4af217a564e7f8a057c71ba2d0f9302786847
labs0lutel <150788736+labs0lutel@users.noreply.github.com>
2024-09-22 21:40:22 +0300

    1

--- js/ava.js
+++ js/ava.js
@@ -1,15 +1,16 @@
 const tg = window.Telegram.WebApp;
 
-        if (tg.initDataUnsafe.user) {
-            const user = tg.initDataUnsafe.user;
-            
-            document.getElementById('userName').textContent = user.first_name;
+if (tg.initDataUnsafe && tg.initDataUnsafe.user) {
+    const user = tg.initDataUnsafe.user;
 
-            if (user.photo_url) {
-                const userPhoto = document.getElementById('userPhoto');
-                userPhoto.src = user.photo_url; 
-                userPhoto.style.display = 'block'; 
-            }
-        } else {
-            document.getElementById('userName').textContent = "Гость";
-        }
+    document.getElementById('userName').textContent = user.first_name;
+
+    if (user.photo_url) {
+        const userPhoto = document.getElementById('userPhoto');
+        userPhoto.src = user.photo_url;
+        userPhoto.style.display = 'block';
+    }
+} else {
+    document.getElementById('userName').textContent = "Гость";
+    console.log("Данные пользователя не найдены.");
+}
