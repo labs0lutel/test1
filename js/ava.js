6efdde065c1f232edf8bc9ca452c0cdfbdac02cc
labs0lutel <150788736+labs0lutel@users.noreply.github.com>
2024-09-22 20:35:39 +0300

    1

---
+++ js/ava.js
@@ -0,0 +1,15 @@
+const tg = window.Telegram.WebApp;
+
+        if (tg.initDataUnsafe.user) {
+            const user = tg.initDataUnsafe.user;
+            
+            document.getElementById('userName').textContent = user.first_name;
+
+            if (user.photo_url) {
+                const userPhoto = document.getElementById('userPhoto');
+                userPhoto.src = user.photo_url; 
+                userPhoto.style.display = 'block'; 
+            }
+        } else {
+            document.getElementById('userName').textContent = "Гость";
+        }
