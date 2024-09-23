276ea101621e735b0ef6cea530d36888c603a8b7
labs0lutel <150788736+labs0lutel@users.noreply.github.com>
2024-09-23 22:56:48 +0300

    1

---
+++ js/click.js
@@ -0,0 +1,39 @@
+const circle = document.getElementById('circle');
+const balanceElement = document.getElementById('balance');
+const profitClickElement = document.getElementById('profitClick');
+const nextStatusElement = document.getElementById('nextStatus');
+const profitHourElement = document.getElementById('profitHour');
+
+    
+let balance = parseInt(localStorage.getItem('balance')) || 0;
+let pointsPerClick = 1;
+let multiplierThreshold = 100000;
+let upgradeCount = parseInt(localStorage.getItem('upgradeCount')) || 0;
+const maxUpgrades = 10;
+
+function updateBalance() {
+    balanceElement.textContent = `Баланс: ${balance} Coins`;
+    profitClickElement.innerHTML = `Прибыль за клик <br>+${pointsPerClick}`;
+    nextStatusElement.textContent = `Следующий статус ${multiplierThreshold}`;
+}
+
+    
+function saveData() {
+    localStorage.setItem('balance', balance);
+    localStorage.setItem('upgradeCount', upgradeCount);
+}
+
+circle.addEventListener('click', () => {
+    balance += pointsPerClick;
+    updateBalance();
+    
+    if (balance >= multiplierThreshold && upgradeCount < maxUpgrades) {
+        pointsPerClick *= 2; 
+        multiplierThreshold *= 2; 
+        upgradeCount++; 
+        updateBalance(); 
+    }
+    saveData(); 
+});
+
+updateBalance();
